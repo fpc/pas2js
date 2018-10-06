abceaab919ffa931d4e44001397c6833f9116e97
michael <michael@freepascal.org>
2018-10-06 12:36:34 +0000

    * Refactored and improved class2pas

--- demo/rtl/democlasstopas.pas
+++ demo/rtl/democlasstopas.pas
@@ -1,65 +1,32 @@
 program democlasstopas;
 
-uses JS, Types, SysUtils;
+uses Web,Classes, JS, class2pas, browserconsole;
 
-function ClassToPas(Obj: TJSObject): string;
-var
-  Names: TStringDynArray;
-  i, j: Integer;
-  t: String;
-  p: TJSArray;
-  f: TJSFunction;
-  Value: JSValue;
+procedure ShowRTLProps(aClassName,aJSClassName : String; O : TJSObject);
+Var
+  S : TStrings;
+  I : Integer;
+
 begin
-  Result:='';
-  p:=TJSArray.new;
-  while Obj<>nil do
-    begin
-    Names:=TJSObject.getOwnPropertyNames(Obj);
-    for i:=0 to length(Names)-1 do
-      begin
-      try
-        Value:=Obj[Names[i]];
-      except
-        Result:=Result+'// not readable property "'+Names[i]+'"'+sLineBreak;
-      end;
-      if jsTypeOf(Value)='function' then
-        begin
-        f:=TJSFunction(Value);
-        t:='function '+f.name+'(';
-        for j:=1 to NativeInt(f['length']) do
-          begin
-          if j>1 then t:=t+';';
-          t:=t+'arg'+IntToStr(j)+' : argtype'+IntToStr(j);
-          end;
-        t:=t+') : returntype;';
-        end
-      else
-        t:=Names[i]+' : vartype;';
-      if p.indexOf(t)<0 then
-        begin
-        p.push(t);
-        Result:=Result+t+sLineBreak;
-        end;
-      end;
-    Obj:=TJSObject.getPrototypeOf(Obj);
-    if Obj<>nil then
-      Result:=Result+'// next getPrototypeOf ...'+sLineBreak;
-    end;
+  S:=TStringList.Create;
+  try
+    ClassToPas(aClassName,aJSClassName,'',O,S,True);
+    For I:=0 to S.Count-1 do
+      Writeln(S[i]);
+  finally
+    S.Free;
+  end;
 end;
 
-procedure ShowRTLProps;
-var
-  o: TJSObject;
+Var
+  o : TJSObject;
+
 begin
   // get the new JavaScript object:
   asm
-  o = window.localStorage; // rtl
+  $mod.o = window.localStorage;
   end;
-  writeln(ClassToPas(o));
-end;
-
-begin
-  ShowRTLProps;
+  MaxConsoleLines:=5000;
+  ShowRTLProps('localStorage','TJSLocalStorage',o);
 end.
 

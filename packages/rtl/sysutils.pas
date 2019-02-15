eff31f67be26dc5b9c383dc77b1f2558d5250bff
mattias <mattias@idefix.freepascal.org>
2019-02-15 22:41:33 +0000

    rtl: fixed AnsiCompareStr

--- packages/rtl/sysutils.pas
+++ packages/rtl/sysutils.pas
@@ -33,9 +33,9 @@ Const
 type
   { TFloatRec }
   TFloatRec = Record
-     Exponent: Integer;
-     Negative: Boolean;
-     Digits: Array[0..FloatRecDigits-1] Of Char;
+    Exponent: Integer;
+    Negative: Boolean;
+    Digits: Array[0..FloatRecDigits-1] of Char;
   End;
   TEndian = (Little,Big);
   TFileName = String;
@@ -167,7 +167,6 @@ type
 
   ENoConstructException = class(Exception);
 
-
 //function GetTickCount: Integer;
 
 
@@ -176,10 +175,10 @@ type
 *****************************************************************************}
 
 Const
-   EmptyStr = '';
-   EmptyWideStr = ''; // No difference here.
-   HexDisplayPrefix: string = '$';
-   LeadBytes = [] unimplemented;
+  EmptyStr = '';
+  EmptyWideStr = ''; // No difference here.
+  HexDisplayPrefix: string = '$';
+  LeadBytes = [] unimplemented;
 
 Function CharInSet(Ch: Char;Const CSet : array of char) : Boolean;
 
@@ -1413,7 +1412,7 @@ end;
 function AnsiCompareStr(const s1, s2: String): Integer;
 begin
   {$IFDEF ECMAScript6}
-  Result:=CompareText(TJSString(s1).normalize(),TJSString(s1).normalize());
+  Result:=CompareText(TJSString(s1).normalize(),TJSString(s2).normalize());
   {$ELSE}
   Result:=CompareText(s1,s2);
   {$ENDIF}
@@ -1432,8 +1431,6 @@ Const
   feInvalidArgIndex = 3;
 
 Procedure DoFormatError (ErrCode : Longint;const fmt: String);
-
-
 begin
   //!! must be changed to contain format string...
   Case ErrCode of

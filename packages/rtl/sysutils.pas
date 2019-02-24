ebdaed71492e4043ebce279fbe00a737741fa25d
mattias <mattias@idefix.freepascal.org>
2019-02-24 09:43:52 +0000

    rtl: fixed Min/MaxCurrency

--- packages/rtl/sysutils.pas
+++ packages/rtl/sysutils.pas
@@ -264,8 +264,8 @@ const
   // The below values are the safe limits, within every step exists.
   // Since currency is a double it can take much larger values, but the result
   // may differ from Delphi/FPC
-  MaxCurrency: Currency =  450359962737.0495; // fpc: 922337203685477.5807;
-  MinCurrency: Currency = -450359962737.0496; // fpc: -922337203685477.5808;
+  MaxCurrency: Currency =  900719925474.0991; // fpc: 922337203685477.5807;
+  MinCurrency: Currency = -900719925474.0991; // fpc: -922337203685477.5808;
 
 Type
   TFloatFormat = (ffFixed,ffGeneral,ffExponent,ffNumber,ffCurrency);

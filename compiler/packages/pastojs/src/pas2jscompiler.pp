1cf4571e9c67354b442e94d3930a8fe46bf6db67
mattias <mattias@idefix.freepascal.org>
2019-03-03 13:25:02 +0000

    set pas2js fixes version 1.4.0RC4

--- compiler/packages/pastojs/src/pas2jscompiler.pp
+++ compiler/packages/pastojs/src/pas2jscompiler.pp
@@ -45,7 +45,7 @@ const
   VersionMajor = 1;
   VersionMinor = 4;
   VersionRelease = 0;
-  VersionExtra = 'RC3';
+  VersionExtra = 'RC4';
   DefaultConfigFile = 'pas2js.cfg';
 
 //------------------------------------------------------------------------------

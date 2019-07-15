021964c4d0aa28fde136690819c3188dca5f1e71
michael <michael@freepascal.org>
2019-07-15 13:14:26 +0000

    * Copyright header added

--- packages/rtl/rstranslate.pp
+++ packages/rtl/rstranslate.pp
@@ -1,3 +1,15 @@
+{
+    This file is part of the Pas2JS run time library.
+    Copyright (c) 2019 by Michael Van Canneyt
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************} 
 unit rstranslate;
 
 {$mode objfpc}

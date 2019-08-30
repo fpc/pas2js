0ca3b5f4be6e4115cefa6275d28c4d180233fea4
mattias <mattias@idefix.freepascal.org>
2019-08-30 09:21:06 +0000

    pastojs: added AlwaysRequireSharedBaseFolder to TryCreateRelativePath

--- compiler/packages/pastojs/src/pas2jscompiler.pp
+++ compiler/packages/pastojs/src/pas2jscompiler.pp
@@ -2240,7 +2240,7 @@ begin
     MapFilename:=LocalFilename;
     if (BaseDir<>'') and not SrcMapFilenamesAbsolute then
     begin
-      if not FS.TryCreateRelativePath(LocalFilename,BaseDir,true,MapFilename) then
+      if not FS.TryCreateRelativePath(LocalFilename,BaseDir,true,false,MapFilename) then
       begin
         // e.g. file is on another partition
         if not SrcMapInclude then

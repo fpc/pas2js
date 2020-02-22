ddc5776e47c04732c5526311a409e9fbf378b748
michael <michael@freepascal.org>
2020-02-22 23:04:23 +0000

    * license added

--- packages/nodejs/nodejsonoff.pas
+++ packages/nodejs/nodejsonoff.pas
@@ -1,3 +1,18 @@
+{
+    This file is part of the Pas2JS run time library.
+    Copyright (c) 2020 by Michael Van Canneyt
+
+    NodeJS onoff module import.
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+
 unit nodejsonoff;
 
 {$mode objfpc}

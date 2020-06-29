66bec813365a8e75850215e8e38b2d86b7d366a9
mattias <mattias@idefix.freepascal.org>
2020-06-29 21:13:31 +0000

    pas2js: fixed try exit(value) finally read Result end

--- compiler/packages/fcl-passrc/src/pasresolver.pp
+++ compiler/packages/fcl-passrc/src/pasresolver.pp
@@ -1176,6 +1176,7 @@ type
   TPRResolveVarAccesses = set of TResolvedRefAccess;
 
 const
+  rraAllRead = [rraRead,rraReadAndAssign,rraVarParam];
   rraAllWrite = [rraAssign,rraReadAndAssign,rraVarParam,rraOutParam];
 
   ResolvedToPSRefAccess: array[TResolvedRefAccess] of TPSRefAccess = (

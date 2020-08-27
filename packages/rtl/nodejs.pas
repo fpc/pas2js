b91651683cdd91a18187ea6737fe27ba335d9029
michael <michael@freepascal.org>
2020-08-27 12:09:51 +0000

    * SPlit eventemitter, often encountered in browser code

--- packages/rtl/nodejs.pas
+++ packages/rtl/nodejs.pas
@@ -18,7 +18,7 @@ unit NodeJS;
 interface
 
 uses
-  JS, Types;
+  JS, Types, node.events;
 
 type
 
@@ -250,34 +250,10 @@ type
     procedure warn(const Obj1 : JSValue); varargs;
   end;
 
-  TNJSEventEmitterHandler = reference to procedure(arg : JSValue);
-  TNJSEventEmitterHandlerArray = array of TNJSEventEmitterHandler;
-
-  TNJSEventEmitter = class external name 'EventEmitter' (TJSObject)
-    class var captureRejections : Boolean;
-    class var defaultMaxListeners : Integer;
-    class var errorMonitor : TNJSEventEmitter;
-    function addListener(const EventName : String; Listener : TNJSEventEmitterHandler) : TNJSEventEmitter;
-    function on_(const EventName : String; Listener : TNJSEventEmitterHandler) : TNJSEventEmitter;
-    function once(const EventName : String; Listener : TNJSEventEmitterHandler) : TNJSEventEmitter;
-    function off(const EventName : String; Listener : TNJSEventEmitterHandler) : TNJSEventEmitter;
-    function emit(const EventName : String) : Boolean; varargs;
-    function eventnames : TJSStringDynArray;
-    function getMaxListeners : Integer;
-    function listenerCount(const EventName : String) : Integer;
-    function listeners(const EventName : String) : TNJSEventEmitterHandlerArray;
-    function prependListener(const EventName : String; Listener : TNJSEventEmitterHandler) : TNJSEventEmitter;
-    function prependOnceListener(const EventName : String; Listener : TNJSEventEmitterHandler) : TNJSEventEmitter;
-    function removeListener(const EventName : String; Listener : TNJSEventEmitterHandler) : TNJSEventEmitter;
-    function setMaxListeners(aMax : Integer): TNJSEventEmitter;
-    function rawListeners(const EventName : String) : TNJSEventEmitterHandlerArray;
-  end;
-
-
-  TNJSEvents = class external name 'events' (TJSObject)
-    function once(emitter : TNJSEventEmitter; aName : string) : TJSPromise;
-    function on_(emitter : TNJSEventEmitter; aName : string) : TJSAsyncIterator;
-  end;
+  TNJSEventEmitterHandler = node.events.TNJSEventEmitterHandler;
+  TNJSEventEmitterHandlerArray = node.events.TNJSEventEmitterHandlerArray;
+  TNJSEventEmitter = node.events.TNJSEventEmitter;
+  TNJSEvents = node.events.TNJSEventEmitter;
 
   TNJSTimerCallBack = reference to procedure;
 

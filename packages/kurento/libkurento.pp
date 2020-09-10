d903f347ec96abc93308541ddf72bff3e4197197
michael <michael@freepascal.org>
2020-09-10 06:53:20 +0000

    * License

--- packages/kurento/libkurento.pp
+++ packages/kurento/libkurento.pp
@@ -1,3 +1,18 @@
+{
+    This file is part of the Pas2JS run time library.
+    Copyright (C) 2020 Michael Van Canneyt
+
+    Kurento import classes for pas2js
+
+    See the file COPYING.FPC, included in this distribution,
+    for details about the copyright.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+ **********************************************************************}
+ 
 unit libkurento;
 
 {$mode objfpc}

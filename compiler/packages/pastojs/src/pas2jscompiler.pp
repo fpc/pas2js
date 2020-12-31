f1bb7ae595bde932663c8b44fee77ebf9d12f7a9
mattias <mattias@idefix.freepascal.org>
2020-12-31 10:29:33 +0000

    set pas2js fixes version 2.0.0.RC8

--- compiler/packages/pastojs/src/pas2jscompiler.pp
+++ compiler/packages/pastojs/src/pas2jscompiler.pp
@@ -46,7 +46,7 @@ const
   VersionMajor = 2;
   VersionMinor = 0;
   VersionRelease = 0;
-  VersionExtra = 'RC7';
+  VersionExtra = 'RC8';
   DefaultConfigFile = 'pas2js.cfg';
 
 //------------------------------------------------------------------------------
